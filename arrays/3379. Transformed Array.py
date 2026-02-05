7acf628f0e292c8427d8be35769fd8f80a854f92
avinaykalyanreddy <avinaykalyanreddy@gmail.com>
2026-02-05 08:46:45 +0530

    3379. Transformed Array

---
+++ arrays/3379. Transformed Array.py
@@ -0,0 +1,40 @@
+"""
+You are given an integer array nums that represents a circular array. Your task is to create a new array result of the same size, following these rules:
+
+For each index i (where 0 <= i < nums.length), perform the following independent actions:
+If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] < 0: Start at index i and move abs(nums[i]) steps to the left in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] == 0: Set result[i] to nums[i].
+Return the new array result.
+
+Note: Since nums is circular, moving past the last element wraps around to the beginning, and moving before the first element wraps back to the end.
+
+
+"""
+
+nums = [3,-2,1,1]
+
+n = len(nums)
+new_arr = nums*3
+res = []
+
+for i in range(n,n*2):
+
+    if new_arr[i] == 0:
+
+        res.append(0)
+
+    else:
+
+        idx = abs(new_arr[i]) % (n)
+
+        if new_arr[i] < 0:
+
+            res.append(new_arr[i-idx])
+
+        else:
+
+            res.append(new_arr[idx+i])
+
+print(res)
+
