61f863087b7639a1ab2a142c0b59bcc4f11fce50
avinaykalyanreddy <avinaykalyanreddy@gmail.com>
2026-02-24 22:01:12 +0530

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ two_pointers/1750. Minimum Length of String After Deleting Similar Ends.py
@@ -0,0 +1,27 @@
+"""
+Given a string s consisting only of characters 'a', 'b', and 'c'. You are asked to apply the following algorithm on the string any number of times:
+
+Pick a non-empty prefix from the string s where all the characters in the prefix are equal.
+Pick a non-empty suffix from the string s where all the characters in this suffix are equal.
+The prefix and the suffix should not intersect at any index.
+The characters from the prefix and suffix must be the same.
+Delete both the prefix and the suffix.
+Return the minimum length of s after performing the above operation any number of times (possibly zero times).
+"""
+s = "cabaabac"
+
+low = 0
+high = len(s)-1
+
+while low < high and s[low] == s[high]:
+            ch = s[low]
+            while low <= high and s[low] == ch:
+
+                low += 1
+
+            while low <= high and s[high] == ch:
+
+                high -= 1
+
+print(high - low + 1)
+
